999d937ee8da892958120edd97834519e885a3bb
Seif <seifeddine.zaddem@external.grtgaz.com>
2023-12-04 09:55:04 +0100

    Refactoring code, implementing smart/dumb component pattern for reservations

--- src/app/core/component/reservation/reservation.component.ts
+++ src/app/core/component/reservation/reservation.component.ts
@@ -1,6 +1,18 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ClientModel} from '../../model/client.model';
+import {MatDialog} from '@angular/material/dialog';
+import {CreditCardPaymentModalComponent} from '../credit-card-payment-modal/credit-card-payment-modal.component';
+import {catchError, EMPTY, Subject, switchMap, tap} from 'rxjs';
+import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
+import {PaypalPaymentModalComponent} from '../paypal-payment-modal/paypal-payment-modal.component';
+import {ReservationModel} from '../../model/reservation.model';
+import {ReservationService} from '../../service/reservation.service';
+import {BillModel} from '../../model/bill.model';
+import {BillService} from '../../service/bill.service';
+import {ReservedJourneyModel} from '../../model/journey.model';
+import {JourneyService} from '../../service/journey.service';
 
+@UntilDestroy()
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.component.html',
@@ -10,11 +22,120 @@ export class ReservationComponent implements OnInit {
 
   @Input()
   client: ClientModel;
-  
-  constructor() {
+  public reservation: ReservationModel;
+  createBill$ = new Subject<BillModel>();
+  updateReservation$ = new Subject<void>();
+  reloadUnpaidReservations$ = new Subject<void>();
+
+  constructor(private reservationService: ReservationService,
+              private billService: BillService,
+              private journeyService: JourneyService,
+              private matDialog: MatDialog) {
   }
 
   ngOnInit(): void {
+    this.updateReservation$.pipe(
+      switchMap(() => this.reservationService.updateReservation(this.reservation)),
+      tap(() => this.reloadUnpaidReservations$.next()),
+      catchError(() => {
+        return EMPTY;
+      }),
+      untilDestroyed(this)
+    ).subscribe();
+
+    this.reloadUnpaidReservations$.pipe(
+      switchMap(() => this.reservationService.getReservationsByStatus('UNPAID', this.client.id)),
+      tap(reservations => {
+        // We get the first element as we consider that there is only one unpaid reservation and each journey adds up to the current reservation
+        if (reservations.length != 0) {
+          this.reservation = reservations[0];
+          this.reservation.totalPrice = this.calculateTotalPrice();
+        } else {
+          this.reservation = null;
+        }
+      }),
+      catchError(() => {
+        return EMPTY;
+      }),
+      untilDestroyed(this)
+    ).subscribe();
+
+    this.reloadUnpaidReservations$.next();
+
+    this.reservationService.getUnpaidReservationsNotification().pipe(
+      tap(() => this.reloadUnpaidReservations$.next()),
+      untilDestroyed(this)
+    ).subscribe();
+
+    this.createBill$.pipe(
+      switchMap(billModel => this.billService.createBill(billModel)),
+    ).subscribe();
+  }
+
+  openCreditCardPaymentModal() {
+    this.matDialog.open(CreditCardPaymentModalComponent, {
+      width: '50%',
+      height: '50%',
+      data: {
+        reservationModel: this.reservation
+      }
+    }).afterClosed().pipe(
+      tap(value => {
+        if (value) {
+          this.reservation.status = 'PAID';
+          this.createBill$.next({
+            reservation: this.reservation,
+            paymentMethod: 'CARD'
+          });
+          this.updateReservation$.next();
+        }
+      }),
+      untilDestroyed(this)
+    ).subscribe();
+  }
+
+  openPaypalPaymentModal() {
+    this.matDialog.open(PaypalPaymentModalComponent, {
+      width: '50%',
+      height: '50%',
+      data: {
+        reservationModel: {
+          ...this.reservation,
+          totalPrice: this.reservation.totalPrice - (this.reservation.totalPrice * 0.05)
+        }
+      }
+    }).afterClosed().pipe(
+      tap(value => {
+        if (value) {
+          this.reservation.status = 'PAID';
+          this.reservation.totalPrice -= (this.reservation.totalPrice * 0.05);
+          this.createBill$.next({
+            reservation: this.reservation,
+            paymentMethod: 'PAYPAL'
+          });
+          this.updateReservation$.next();
+        }
+      }),
+      untilDestroyed(this)
+    ).subscribe();
+  }
+
+  deleteJourney(reservedJourneyModel: ReservedJourneyModel) {
+    this.reservationService.deleteReservedJourney(reservedJourneyModel).pipe(
+      tap(() => {
+        this.reloadUnpaidReservations$.next();
+        this.journeyService.notifyJourney();
+      }),
+      untilDestroyed(this)
+    ).subscribe();
+  }
+
+  calculateTotalPrice(): number {
+
+    return this.reservation ? this.reservation?.reservedJourneys.reduce((total, reservedJourney) => {
+      total += reservedJourney.journey.price * reservedJourney.seats;
+      return total;
+    }, 0) : 0;
   }
 
 }
--- src/app/core/component/unpaid-reservation/unpaid-reservation.component.ts
+++ src/app/core/component/unpaid-reservation/unpaid-reservation.component.ts
@@ -1,18 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {ReservationService} from '../../service/reservation.service';
-import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {ReservationModel} from '../../model/reservation.model';
-import {ClientModel} from '../../model/client.model';
-import {catchError, EMPTY, Subject, switchMap, tap} from 'rxjs';
 import {ReservedJourneyModel} from '../../model/journey.model';
-import {JourneyService} from '../../service/journey.service';
-import {MatDialog} from '@angular/material/dialog';
-import {CreditCardPaymentModalComponent} from '../credit-card-payment-modal/credit-card-payment-modal.component';
-import {BillService} from '../../service/bill.service';
-import {BillModel} from '../../model/bill.model';
-import {PaypalPaymentModalComponent} from '../paypal-payment-modal/paypal-payment-modal.component';
 
-@UntilDestroy()
 @Component({
   selector: 'app-pending-reservation',
   templateUrl: './unpaid-reservation.component.html',
@@ -20,128 +9,38 @@ import {PaypalPaymentModalComponent} from '../paypal-payment-modal/paypal-paymen
 })
 export class UnpaidReservationComponent implements OnInit {
 
-  public reservation: ReservationModel;
-  reloadUnpaidReservations$ = new Subject<void>();
-  updateReservation$ = new Subject<void>();
-  createBill$ = new Subject<BillModel>();
   @Input()
-  client: ClientModel;
+  public reservation: ReservationModel;
   displayedColumns = ["departure", "arrival", "date", "pricePerSeat", "seats", "totalPrice", "delete"];
 
-  constructor(private reservationService: ReservationService,
-              private billService: BillService,
-              private matDialog: MatDialog,
-              private journeyService: JourneyService) {
+  @Output()
+  OnDeleteJourney = new EventEmitter<ReservedJourneyModel>();
+  @Output()
+  onCreditCardPayment = new EventEmitter<void>();
+  @Output()
+  onPaypalPayment = new EventEmitter<void>();
+
+  constructor() {
   }
 
   ngOnInit(): void {
 
-    this.updateReservation$.pipe(
-      switchMap(() => this.reservationService.updateReservation(this.reservation)),
-      tap(() => this.reloadUnpaidReservations$.next()),
-      catchError(() => {
-        return EMPTY;
-      }),
-      untilDestroyed(this)
-    ).subscribe();
-
-    this.reloadUnpaidReservations$.pipe(
-      switchMap(() => this.reservationService.getReservationsByStatus('UNPAID', this.client.id)),
-      tap(reservations => {
-        // We get the first element as we consider that there is only one unpaid reservation and each journey adds up to the current reservation
-        if (reservations.length != 0) {
-          this.reservation = reservations[0];
-          this.reservation.totalPrice = this.calculateTotalPrice();
-        } else {
-          this.reservation = null;
-        }
-      }),
-      catchError(() => {
-        return EMPTY;
-      }),
-      untilDestroyed(this)
-    ).subscribe();
-
-    this.reloadUnpaidReservations$.next();
-
-    this.reservationService.getUnpaidReservationsNotification().pipe(
-      tap(() => this.reloadUnpaidReservations$.next()),
-      untilDestroyed(this)
-    ).subscribe();
-
-    this.createBill$.pipe(
-      switchMap(billModel => this.billService.createBill(billModel)),
-    ).subscribe();
   }
 
   deleteJourney(reservedJourneyModel: ReservedJourneyModel) {
-    this.reservationService.deleteReservedJourney(reservedJourneyModel).pipe(
-      tap(() => {
-        this.reloadUnpaidReservations$.next();
-        this.journeyService.notifyJourney();
-      }),
-      untilDestroyed(this)
-    ).subscribe();
-  }
-
-  calculateTotalPrice(): number {
-
-    return this.reservation ? this.reservation?.reservedJourneys.reduce((total, reservedJourney) => {
-      total += reservedJourney.journey.price * reservedJourney.seats;
-      return total;
-    }, 0) : 0;
-  }
-
-  openCreditCardPaymentModal() {
-    this.matDialog.open(CreditCardPaymentModalComponent, {
-      width: '50%',
-      height: '50%',
-      data: {
-        reservationModel: this.reservation
-      }
-    }).afterClosed().pipe(
-      tap(value => {
-        if (value) {
-          this.reservation.status = 'PAID';
-          this.createBill$.next({
-            reservation: this.reservation,
-            paymentMethod: 'CARD'
-          });
-          this.updateReservation$.next();
-        }
-      }),
-      untilDestroyed(this)
-    ).subscribe();
-  }
-
-  openPaypalPaymentModal() {
-    this.matDialog.open(PaypalPaymentModalComponent, {
-      width: '50%',
-      height: '50%',
-      data: {
-        reservationModel: {
-          ...this.reservation,
-          totalPrice: this.reservation.totalPrice - (this.reservation.totalPrice * 0.05)
-        }
-      }
-    }).afterClosed().pipe(
-      tap(value => {
-        if (value) {
-          this.reservation.status = 'PAID';
-          this.reservation.totalPrice -= (this.reservation.totalPrice * 0.05);
-          this.createBill$.next({
-            reservation: this.reservation,
-            paymentMethod: 'PAYPAL'
-          });
-          this.updateReservation$.next();
-        }
-      }),
-      untilDestroyed(this)
-    ).subscribe();
+    this.OnDeleteJourney.emit(reservedJourneyModel);
   }
 
 
   areJourneysReserved(): boolean {
     return this.reservation?.reservedJourneys != undefined && this.reservation.reservedJourneys.length != 0;
+  }
+
+  openCreditCardPaymentModal() {
+    this.onCreditCardPayment.emit();
+  }
+
+  openPaypalPaymentModal() {
+    this.onPaypalPayment.emit();
   }
 }
